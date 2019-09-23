3bb96156f6356b77198eed5c2472155644179305
Felipe <felipeparreirafb@gmail.com>
2019-09-23 20:51:23 -0300

    Heroku config files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=4g++-^i@g=k_+t3v=%-&w&7gt&f48&@gb9q%$==-vydn0_r#i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
